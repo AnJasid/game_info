c7fc604f986f98aebd196235b34a6bb90ec4d6b3
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-26 20:59:55 +0800

    create sport data

---
+++ lib/sport_list/sport_data.dart
@@ -0,0 +1,14 @@
+import 'package:game_info/models/sport.dart';
+
+class SportData {
+  final List<Sport> sportList = [
+    const Sport(
+      title: 'Cricket',
+      text: 'text',
+    ),
+    const Sport(
+      title: 'Football',
+      text: 'text',
+    ),
+  ];
+}
