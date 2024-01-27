3f54804697e11cbf7590122b11131ff40ca15133
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-27 16:58:51 +0800

    update for sport item

--- lib/sport_list/sport_item.dart
+++ lib/sport_list/sport_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game_info/models/sport.dart';
+import 'package:game_info/screens/info_screen.dart';
 
 class SportItem extends StatelessWidget {
   const SportItem({
@@ -9,15 +10,28 @@ class SportItem extends StatelessWidget {
 
   final Sport sport;
 
+  void _goToInfoScreen(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const InfoScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Container(
-        height: 200,
-        width: 200,
-        decoration: const BoxDecoration(
-          color: Colors.black,
+      child: GestureDetector(
+        onTap: () {
+          _goToInfoScreen(context);
+        },
+        child: Container(
+          height: 200,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Colors.black,
+          ),
         ),
       ),
     );
