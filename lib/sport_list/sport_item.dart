85662d7b5f29f07212c535fa20494003c0e7fd0d
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-29 16:56:52 +0800

    update for sport item

--- lib/sport_list/sport_item.dart
+++ lib/sport_list/sport_item.dart
@@ -21,79 +21,58 @@ class SportItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 25),
+      // ignore: prefer_const_constructors
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 25,
+      ),
       child: GestureDetector(
-        onTap: () {
-          _goToInfoScreen(context);
-        },
+        onTap: () => _goToInfoScreen(context),
         child: Container(
-          height: 180,
+          // height: 200,
           decoration: BoxDecoration(
-            // gradient: const LinearGradient(
-            //   begin: Alignment.topLeft,
-            //   end: Alignment.topRight,
-            //   colors: [
-            //     Color(0xffF5B7B1),
-            //     Color(0xffE7D3D3),
-            //   ],
-            // ),
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.topRight,
+              colors: [
+                Color(0xffD0E9F5),
+                Color(0xffA9CCE3),
+              ],
+            ),
             borderRadius: BorderRadius.circular(15),
             border: Border.all(
-              color: Colors.white,
-              width: 2.0,
+              color: Colors.amber,
+              width: 1.5,
             ),
           ),
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.asset(
-                  sport.imagePath,
-                  fit: BoxFit.fill,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Column(
+              children: [
+                Container(
+                  width: 260,
+                  height: 150,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              // ignore: prefer_const_constructors
-              Positioned(
-                bottom: 1,
-                left: 20,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Name',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          'View Info',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                  ],
-                ),
-              )
-            ],
+                const SizedBox(height: 5),
+                const Text('Name'),
+                const SizedBox(height: 5),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text(
+                    'View Info',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
           ),
-          // child: Center(
-          //   child: Container(
-          //     width: 150,
-          //     height: 150,
-          //     child: Image.asset(sport.imagePath),
-          //   ),
-          // ),
         ),
       ),
     );
