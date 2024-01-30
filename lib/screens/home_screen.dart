3487cc9790cda0a30bb1305baabcab65924c44a2
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-30 20:27:24 +0800

    update for home screen: resource button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:game_info/screens/list_screen.dart';
+import 'package:game_info/utils/button_utils.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -35,34 +36,14 @@ class HomeScreen extends StatelessWidget {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: 45,
-              width: 120,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                gradient: const LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(0xffD0E9F5),
-                    Color(0xffA9CCE3),
-                  ],
-                ),
-              ),
-              child: const Center(
-                child: Text('Start'),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _goToListScreen(context);
-              },
-              child: const Text('Start'),
+            ButtonUtils(
+              onTap: () => _goToListScreen(context),
+              buttonText: 'Start',
             ),
             const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _exitApp,
-              child: const Text('Exit'),
+            ButtonUtils(
+              onTap: _exitApp,
+              buttonText: 'Exit',
             ),
           ],
         )),
