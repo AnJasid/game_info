3b5abd8cb7f45f5ff1fb5cf97ff6992e3d6c2415
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-30 19:59:03 +0800

    update button utils

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -35,6 +35,24 @@ class HomeScreen extends StatelessWidget {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              height: 45,
+              width: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xffD0E9F5),
+                    Color(0xffA9CCE3),
+                  ],
+                ),
+              ),
+              child: const Center(
+                child: Text('Start'),
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
                 _goToListScreen(context);
--- lib/utils/button_utils.dart
+++ lib/utils/button_utils.dart
@@ -4,28 +4,33 @@ class ButtonUtils extends StatelessWidget {
   const ButtonUtils({
     super.key,
     required this.buttonText,
+    required this.onTap,
   });
 
   final String buttonText;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: 120,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        gradient: const LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0xffD0E9F5),
-            Color(0xffA9CCE3),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 45,
+        width: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          gradient: const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xffD0E9F5),
+              Color(0xffA9CCE3),
+            ],
+          ),
         ),
-      ),
-      child: Center(
-        child: Text(buttonText),
+        child: Center(
+          child: Text(buttonText),
+        ),
       ),
     );
   }
