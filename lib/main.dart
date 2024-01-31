4af4c2bc88b40ab5927849065769c6532f80176c
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-31 12:37:44 +0800

    update main: update app orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:game_info/provider/theme_provider.dart';
 import 'package:game_info/screens/home_screen.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -18,6 +23,7 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isLightTheme = ref.watch(themeProvider);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
       theme: isLightTheme ? ThemeData.light() : ThemeData.dark(),
     );
